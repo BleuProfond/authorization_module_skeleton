d72291554593fa3333573ff8eb0d31db4eb65339
BleuProfond <p.verteuil@gmail.com>
2016-05-18 17:42:42 -0400

    added signin logic

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,9 +3,32 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  // Verify this email and password,
+  User.findOne({ email: email }, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false); }
+
+    // compare `password` to user.password and see if they match
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      // if email and password do not match, call 'done' with false
+      if (!isMatch) { return done(null, false); }
+      // if the email and password are correct, call 'done' with the user
+      return done(null, user);
+    });
+  });
+});
 
 // Setup options for JWT Strategy
-const jwtOptions = {};
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+};
 
 // Create JWT strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
@@ -23,3 +46,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 // Tell passport to use this strategy
+passport.use(jwtLogin);
